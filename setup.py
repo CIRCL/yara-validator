3c554e911d92daba588eab5d17084f68501fef75
edhoedt <edhoedt@gmail.com>
2017-09-13 11:05:53 +0200

    version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'yara_validator',
     packages = ['yara_validator'],
-    version = '0.1',  # Ideally should be same as your GitHub release tag varsion
+    version = 'v0.1',  # Ideally should be same as your GitHub release tag varsion
     description = 'A simple library to check yara rules syntax',
     author = 'CIRCL - edhoedt',
     author_email = 'edhoedt@gmail.com',
